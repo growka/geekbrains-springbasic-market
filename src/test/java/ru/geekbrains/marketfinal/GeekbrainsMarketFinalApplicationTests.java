08cb13e53e9e369e05ecc5c989c5f5bd49dc6031
growka <thegusev@gmail.com>
2021-08-11 15:28:52 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/marketfinal/GeekbrainsMarketFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.marketfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekbrainsMarketFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
